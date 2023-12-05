e80677b37737db7efe74445f836231f4eb42b035
Kevin Hou <strawberrypi64@gmail.com>
2023-12-05 18:35:23 +1100

    frontend working

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,8 +15,8 @@ export default function Home() {
                 <div className="mt-2">
                   <input
                     type="text"
-                    name="first-name"
-                    id="first-name"
+                    name="team-number"
+                    id="team-number"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6"
                   />
                 </div>
@@ -49,35 +49,18 @@ export default function Home() {
                 </div>
               </div>
 
-              <label className="block text-sm font-medium leading-6 text-gray-900">
+              <label className="block text-sm font-medium text-gray-900">
                 Autonomous Position
               </label>
               <div className="mt-2">
                 <select
-                  id="Autonomous Position"
-                  name="Autonomous Position"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm  sm:max-w-xs sm:text-sm sm:leading-6"
+                  id="autonomous-position"
+                  name="autonomous-position"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm"
                 >
                   <option>Towards Backboard</option>
                   <option>Towards Landing Zone</option>
                 </select>
-
-                <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                  <div className="col-span-full">
-                    <label className="block text-sm font-medium leading-6 text-gray-900">
-                      Additional Notes
-                    </label>
-                    <div className="mt-2">
-                      <textarea
-                        id="notes"
-                        name="notes"
-                        rows={10}
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6"
-                        defaultValue={""}
-                      />
-                    </div>
-                  </div>
-                </div>
               </div>
 
               <div className="sm:col-span-3">
@@ -88,7 +71,7 @@ export default function Home() {
                   <select
                     id="drone"
                     name="drone"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm  sm:max-w-xs sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm sm:leading-6"
                   >
                     <option>N/A (+0 Points)</option>
                     <option>Landing Zone 1 (+30 Points)</option>
@@ -98,38 +81,51 @@ export default function Home() {
                 </div>
               </div>
 
-              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                <div className="sm:col-span-3">
-                  <label className="block text-sm font-medium leading-6 text-gray-900">
-                    Team Number
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      type="text"
-                      name="first-name"
-                      id="first-name"
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6"
-                    />
-                  </div>
+              <div className="sm:col-span-3">
+                <label className="block text-sm font-medium leading-6 text-gray-900">
+                  Climb Time
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="climb-time"
+                    id="climb-time"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                  />
                 </div>
               </div>
             </div>
           </div>
-
-          <div className="mt-6 flex items-center justify-end gap-x-6">
-            <button
-              type="button"
-              className="text-sm font-semibold leading-6 text-gray-900"
-            >
-              Cancel
-            </button>
-            <button
-              type="submit"
-              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500"
-            >
-              Save
-            </button>
+        </div>
+        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="col-span-full">
+            <label className="block text-sm font-medium leading-6 text-gray-900">
+              Additional Notes
+            </label>
+            <div className="mt-2">
+              <textarea
+                id="notes"
+                name="notes"
+                rows={10}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                defaultValue={""}
+              />
+            </div>
           </div>
+        </div>
+        <div className="mt-6 flex items-center justify-end gap-x-6">
+          <button
+            type="button"
+            className="text-sm font-semibold leading-6 text-gray-900"
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500"
+          >
+            Save
+          </button>
         </div>
       </div>
     </form>
