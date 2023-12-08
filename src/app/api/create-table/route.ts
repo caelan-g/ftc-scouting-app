7dd80d67fcf1937200a6a49f0698aed255a16b8b
caelan-g <141709327+caelan-g@users.noreply.github.com>
2023-12-08 21:32:39 +1100

    prisma init

--- src/app/api/create-table/route.ts
+++ src/app/api/create-table/route.ts
@@ -1,15 +1,18 @@
 import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 export async function GET(request: Request) {
   try {
-    const result = await sql`CREATE TABLE TeamData ( 
-      team-ID INTEGER PRIMARY KEY,
+    const result = await sql`CREATE TABLE RobotData (
+      teamID INTEGER,
       name VARCHAR(255), 
-      climb-success DECIMAL(3,2)
-      drone-avg DECIMAL(2,2)
-      cycle-avg DECIMAL(2,2)
-      autonomous-position VARCHAR(255)
+      climb BOOLEAN,
+      drone BOOLEAN,
+      cycles INTEGER,
+      autonomousPosition VARCHAR(255),
+      notes VARCHAR(255)
     );`;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
@@ -27,3 +30,5 @@ export async function GET(request: Request) {
   climb
   notes
   */
+
+//http://localhost:3000/api/create-table-basic
